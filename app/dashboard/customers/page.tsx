5b2a7929a9c1759c5f073880ea4c19d7ac73e9ce
Owais Ahmad <91786886+owaisahmad315@users.noreply.github.com>
2025-01-25 17:17:04 +0500

    done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
 export default function Customers() {
     return <p>Customers Page</p>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,11 @@ const config: Config = {
           500: '#0070F3',
           600: '#2F6FEB',
         },
+        green: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
       },
     },
     keyframes: {
